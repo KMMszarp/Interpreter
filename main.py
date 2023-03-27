b238c2fbddfa09ca33fe82be684968efa6a730fc
achook <karol@kucza.xyz>
2023-03-27 21:54:13 +0200

    Add support for printing
    Adds support for printing of top level 'napisz' expressions

--- main.py
+++ main.py
@@ -1,20 +1,54 @@
 import sys
 
-from antlr4 import *
+import antlr4
 
 from kmmszarp.kmmszarpLexer import kmmszarpLexer
 from kmmszarp.kmmszarpParser import kmmszarpParser
 
 
+def handle_print(print_expr):
+    print(print_expr.getText())
+
+
+def handle_function_call(call):
+    children = [*call.getChildren()]
+
+    if not isinstance(children[0], antlr4.tree.Tree.TerminalNodeImpl):
+        raise SyntaxError("Expected function call to start with a keyword")
+
+    # Chyba niepotrzebne
+    # assert children[0].getText() == "wywołaj"
+    # assert isinstance(children[1], antlr4.tree.Tree.TerminalNodeImpl)
+
+    function_name = children[1].getText()
+    if function_name == "napisz":
+        handle_print(children[2])
+    else:
+        raise NotImplementedError("Not able to handle function " + function_name)
+
+
+def handle_statement(stmt):
+    for child in stmt.getChildren():
+        if isinstance(child, kmmszarpParser.FunctionCallContext):
+            handle_function_call(child)
+        else:
+            raise NotImplementedError("Not able to handle " + str(type(child)) + " yet")
+
+
 def handle_expression(expr):
     for child in expr.getChildren():
-        print(type(child), child.getText())
+        if isinstance(child, kmmszarpParser.StatementContext):
+            handle_statement(child)
+        elif isinstance(child, antlr4.tree.Tree.TerminalNodeImpl):
+            pass
+        else:
+            raise NotImplementedError("Not able to handle " + str(type(child)) + " yet")
 
 
 def main(argv):
-    input_stream = FileStream(argv[1], encoding="utf-8")
+    input_stream = antlr4.FileStream(argv[1], encoding="utf-8")
     lexer = kmmszarpLexer(input_stream)
-    stream = CommonTokenStream(lexer)
+    stream = antlr4.CommonTokenStream(lexer)
     parser = kmmszarpParser(stream)
     tree = parser.program()
     handle_expression(tree)
