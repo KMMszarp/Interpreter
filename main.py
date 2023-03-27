56ccb21c9eb46764654fdcd5399eaee8106ae790
achook <karol@kucza.xyz>
2023-03-27 22:54:27 +0200

    Prevent quotes from being printed

--- main.py
+++ main.py
@@ -7,7 +7,38 @@ from kmmszarp.kmmszarpParser import kmmszarpParser
 
 
 def handle_print(print_expr):
-    print(print_expr.getText())
+    # Very dirty parsing of the print expression starts here
+
+    # Need to go all the way down to the string
+    # To be on the safe side assert that the structure is as expected
+    expression_context = print_expr.getChild(0)
+    assert isinstance(expression_context, kmmszarpParser.ExpressionContext)
+    logic_or_context = expression_context.getChild(0)
+    assert isinstance(logic_or_context, kmmszarpParser.LogicOrContext)
+    logic_and_context = logic_or_context.getChild(0)
+    assert isinstance(logic_and_context, kmmszarpParser.LogicAndContext)
+    equality_context = logic_and_context.getChild(0)
+    assert isinstance(equality_context, kmmszarpParser.EqualityContext)
+    comparison_context = equality_context.getChild(0)
+    assert isinstance(comparison_context, kmmszarpParser.ComparisonContext)
+    addition_context = comparison_context.getChild(0)
+    assert isinstance(addition_context, kmmszarpParser.AdditionContext)
+    multiplication_context = addition_context.getChild(0)
+    assert isinstance(multiplication_context, kmmszarpParser.MultiplicationContext)
+    primary_context = multiplication_context.getChild(0)
+    assert isinstance(primary_context, kmmszarpParser.PrimaryContext)
+
+    # Check if the primary type is a string indeed
+    full_string = primary_context.STRING()
+    assert full_string is not None
+
+    # Remove the quotes from the string
+    full_string_text = full_string.getText()
+    string = full_string_text[20:-18]
+
+    # Very dirty parsing of the print expression ends here
+
+    print(string)
 
 
 def handle_function_call(call):
