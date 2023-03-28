279ba4c61440c0419b87f8864014e3db143ba8b3
McOwska <zuza.m.makowska@gmail.com>
2023-03-28 20:26:00 +0200

    Handling exceptions during parsing

---
+++ kmmszarpErrorListener.py
@@ -0,0 +1,7 @@
+from antlr4.error.ErrorListener import ErrorListener
+from antlr4.error.Errors import ParseCancellationException
+
+class kmmszarpErrorListener(ErrorListener):
+
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        raise ParseCancellationException("An error occurred while parsing the file. Line: " + str(line))
--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 import sys
 
 import antlr4
+from antlr4.error.Errors import ParseCancellationException
 
 from kmmszarp.kmmszarpLexer import kmmszarpLexer
 from kmmszarp.kmmszarpParser import kmmszarpParser
 from visitor import Visitor
+from kmmszarpErrorListener import kmmszarpErrorListener
 
 
 def main(argv):
-    # Input
-    input_stream = antlr4.FileStream(argv[1], encoding="utf-8")
+    try:
+        # Input
+        input_stream = antlr4.FileStream(argv[1], encoding="utf-8")
+    except Exception as e:
+        print("An error occurred while opening the file")
 
-    # Lexer
-    lexer = kmmszarpLexer(input_stream)
+    try:
+        # Lexer
+        lexer = kmmszarpLexer(input_stream)
 
-    # Parser
-    stream = antlr4.CommonTokenStream(lexer)
-    parser = kmmszarpParser(stream)
-    tree = parser.program()
+        # Parser
+        stream = antlr4.CommonTokenStream(lexer)
+        parser = kmmszarpParser(stream)
+        parser.removeErrorListeners()
+        parser.addErrorListener(kmmszarpErrorListener())
+        tree = parser.program()
+    except ParseCancellationException as e:
+        print(e)
+        return
 
     # Visitor
     visitor = Visitor()
