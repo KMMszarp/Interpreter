73d73ed000f7caa3e006c36f7e1696b618b127a0
McOwska <zuza.m.makowska@gmail.com>
2023-03-28 21:08:51 +0200

    Handling exceptions during parsing - displaying line + column

--- kmmszarpErrorListener.py
+++ kmmszarpErrorListener.py
@@ -4,4 +4,4 @@ from antlr4.error.Errors import ParseCancellationException
 class kmmszarpErrorListener(ErrorListener):
 
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        raise ParseCancellationException("An error occurred while parsing the file. Line: " + str(line))
+        raise ParseCancellationException("An error occurred while parsing the file. Line: " + str(line) + ", column: " + str(column))
